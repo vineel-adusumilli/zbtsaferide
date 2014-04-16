740f92a421243bb710e9cda530bed2ff851fc56c
Vineel Adusumilli <vineel@adusumilli.org>
2014-04-16 00:08:48 -0400

    Now shows display with info.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   var map = L.mapbox.map('map', 'vineel.i06b0oh9').setView([42.35, -71.11], 17);
+  map.legendControl.addLegend(document.getElementById('legend-content').innerHTML);
 
   // Draw Saferide route
   $.get('http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=mit&r=saferidebostonw', function(data) {
@@ -24,10 +25,25 @@ $(document).ready(function() {
     });
   });
 
-  // Function that plots the shuttle
+  // Function that plots the shuttle and updates the times
   var shuttle = L.circleMarker([42.35, -71.11], 
       {fill: true, fillOpacity: 1, radius: 30}).addTo(map);
   function updatePosition() {
+    // Update the times
+    $.get('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=mit&r=saferidebostonw&s=manc58', function(data) {
+      var $xml = $(data);
+      var prediction = $($xml.find('prediction').first());
+
+      $('#time').html('<h2>' + (new Date()).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3") + '</h2>');
+
+      var minutes = parseInt(prediction.attr('minutes'));
+      $('#nextmin').html('<h2>' + minutes + ' minute' + (minutes == 1 ? '' : 's') + '</h2>');
+
+      var nexttime = parseInt(prediction.attr('epochTime'));
+      $('#nexttime').html('<h2>' + (new Date(nexttime)).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3") + '</h2>');
+    });
+
+    // Plot the shuttle
     $.get('http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=mit&r=saferidebostonw&t=0', function(data) {
       var $xml = $(data);
 
